bd8e6443117f9be318c67c40cba1cf658c135c3f
Tyler <yiqizhang@gmail.com>
2020-03-10 19:13:52 +0000

    v1

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -18,14 +18,18 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "navigate from home page to product detail page by clicking on product" do
     # ACT
     visit root_path
+
+    click_on @category.products.first.name
+
+    expect(page).to have_css 'section.products-show'
 
     # DEBUG
     save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    # expect(page).to have_css 'article.product', count: 10
   end
 end
